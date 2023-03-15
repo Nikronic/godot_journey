ecd44b394516c3710dc17fdf2927427c0337857e
Nikronic <nikan.doosti@outlook.com>
2023-03-15 15:59:51 +0330

    add movement on key press

---
+++ first_script/RotatingSprite2D.gd
@@ -0,0 +1,43 @@
+extends Sprite2D
+
+var speed = 400
+var angular_speed = deg_to_rad(90.0)
+var base_mov = 5.0
+
+
+func _process(_delta):
+	# rotate_object(delta)
+	
+	var direction = 0.0
+	var position_change = position
+	
+	if Input.is_action_pressed("ui_left"):
+		direction = Vector2.LEFT
+		position_change = direction * base_mov + position
+	if Input.is_action_pressed("ui_right"):
+		direction = Vector2.RIGHT
+		position_change = direction * base_mov + position
+	if Input.is_action_pressed("ui_up"):
+		direction = Vector2.UP
+		position_change = direction * base_mov + position
+	if Input.is_action_pressed("ui_down"):
+		direction = Vector2.DOWN
+		position_change = direction * base_mov + position
+	
+	position = position_change
+	
+	
+
+func rotate_object(delta):
+	var direction = 0
+	
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+	
+	# rotate via arrow (left, right) keys
+	var rotation_change = 0.0
+	rotation_change = angular_speed * direction
+	rotation += rotation_change * delta
+	
