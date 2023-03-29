2e01f651c8f653e1eae47a7e1def8ff608e9ec34
Nikronic <nikan.doosti@outlook.com>
2023-03-29 11:07:16 +0330

    add movement toggle script

--- first_script/RotatingSprite2D.gd
+++ first_script/RotatingSprite2D.gd
@@ -10,26 +10,29 @@ var move_allowed = true
 func _process(_delta):
 	# rotate_object(delta)
 	
+	if move_allowed:  # if move allowed via button 
+		manual_move_object(_delta)
+	
+
+func manual_move_object(delta):
+		
 	var direction = 0.0
 	var position_change = position
-	
-	if move_allowed:  # if move allowed via button 
-		if Input.is_action_pressed("ui_left"):
-			direction = Vector2.LEFT
-			position_change = direction * base_mov + position
-		if Input.is_action_pressed("ui_right"):
-			direction = Vector2.RIGHT
-			position_change = direction * base_mov + position
-		if Input.is_action_pressed("ui_up"):
-			direction = Vector2.UP
-			position_change = direction * base_mov + position
-		if Input.is_action_pressed("ui_down"):
-			direction = Vector2.DOWN
-			position_change = direction * base_mov + position
-	
+
+	if Input.is_action_pressed("ui_left"):
+		direction = Vector2.LEFT
+		position_change = direction * base_mov + position
+	if Input.is_action_pressed("ui_right"):
+		direction = Vector2.RIGHT
+		position_change = direction * base_mov + position
+	if Input.is_action_pressed("ui_up"):
+		direction = Vector2.UP
+		position_change = direction * base_mov + position
+	if Input.is_action_pressed("ui_down"):
+		direction = Vector2.DOWN
+		position_change = direction * base_mov + position
+
 	position = position_change
-	
-	
 
 func rotate_object(delta):
 	var direction = 0
