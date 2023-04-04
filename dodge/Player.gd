2ac5f1a5eb0107d45e386653b1612684c10eacb6
Nikronic <nikan.doosti@outlook.com>
2023-04-04 17:04:02 +0330

    replace entire mov logic with official doc (mine had no diagonal move, and was buggy)

--- dodge/Player.gd
+++ dodge/Player.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+# screen size to bound the characters
+var screen_size
 
 # player mov speed
-var base_mov = 5.0
+var base_mov_speed = 300.0
 # enable/disable player mov
 var move_allowed = true
 
@@ -18,56 +20,51 @@ func _process(_delta):
 	
 
 func manual_move_object(_delta):
-		
-	var direction = 0.0
-	var position_change = position
+	# The player's movement vector (init = 0)
+	var velocity = Vector2.ZERO
 
 	if Input.is_action_pressed("ui_left"):
-		direction = Vector2.LEFT
-		position_change = direction * base_mov + position
-		# animation
-		animate_sprite(animated_sprite, 'left')
+		velocity.x -= 1.0
+		
 	if Input.is_action_pressed("ui_right"):
-		direction = Vector2.RIGHT
-		position_change = direction * base_mov + position
-		# animation
-		animate_sprite(animated_sprite, 'right')
+		velocity.x += 1.0
+		
 	if Input.is_action_pressed("ui_up"):
-		direction = Vector2.UP
-		position_change = direction * base_mov + position
-		# animation
-		animate_sprite(animated_sprite, 'up')
+		velocity.y -= 1.0
+		
 	if Input.is_action_pressed("ui_down"):
-		direction = Vector2.DOWN
-		position_change = direction * base_mov + position
-		# animation
-		animate_sprite(animated_sprite, 'down')
+		velocity.y += 1.0
 	
-	position = position_change
-	
+	# if player is moving
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * base_mov_speed
+		# animation w.r.t. direction of mov
+		animate_sprite(animated_sprite, velocity)
 	# stop animation if no key pressed
-	if not Input.is_anything_pressed():
+	else:
 		animated_sprite.stop()
+	
+	position = clamp_player_pos(position, screen_size)
+	position += velocity * _delta
 
 # player animation w.r.t. player movement
-func animate_sprite(animated_sprite, dir):
-	if dir == 'left':
-		animated_sprite.flip_h = true
+func animate_sprite(animated_sprite, velocity: Vector2):
+	# left/right
+	if velocity.x != 0:
+		animated_sprite.flip_h = velocity.x < 0
 		animated_sprite.flip_v = false
 		animated_sprite.play("walk")
-	if dir == 'right':
+	# down/up
+	if velocity.y != 0:
 		animated_sprite.flip_h = false
-		animated_sprite.flip_v = false
-		animated_sprite.play("walk")
-	if dir == 'up':
-		animated_sprite.flip_h = false
-		animated_sprite.flip_v = false
-		animated_sprite.play("up")
-	if dir == 'down':
-		animated_sprite.flip_h = false
-		animated_sprite.flip_v = true
+		animated_sprite.flip_v = velocity.y > 0
 		animated_sprite.play("up")
 
-	
+# prevent player from moving outside of the screen
+func clamp_player_pos(position, screen_size):
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	return position
+
 func _ready():
-	pass
+	screen_size = get_viewport_rect().size
